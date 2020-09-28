bc407317a9df596152b261e01fe55daf167dbe83
SVitya <s.vitya96@gmail.com>
2020-09-28 22:02:05 +0300

    Updates

---
+++ precache-manifest.2da86679456ed59697253666c24d654a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4027271e29291d81783daf1a6813a9db",
+    "url": "/Reservation-app/index.html"
+  },
+  {
+    "revision": "7988af230dfd363c917c",
+    "url": "/Reservation-app/static/css/main.2f21eb86.chunk.css"
+  },
+  {
+    "revision": "a1c85b73fc2648edf613",
+    "url": "/Reservation-app/static/js/2.e177f8dd.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Reservation-app/static/js/2.e177f8dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7988af230dfd363c917c",
+    "url": "/Reservation-app/static/js/main.682548fd.chunk.js"
+  },
+  {
+    "revision": "0f2a7a7bf5c3b29a3d17",
+    "url": "/Reservation-app/static/js/runtime-main.a32cf3b2.js"
+  },
+  {
+    "revision": "18d65fccf06d08ae69d4bd65d7f685c8",
+    "url": "/Reservation-app/static/media/bg.18d65fcc.jpg"
+  }
+]);
